bceecaed87fedbb4f6c395ba9e3fd6db25e9e31b
GonzaloFrascheri <gfrascheri@Gonzalo.localdomain>
2023-05-22 21:02:48 -0300

    JournalApp - Seccion 17 terminada

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,33 @@
-import { Typography } from "@mui/material"
+import { IconButton } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView } from "../views/NoteView"
+import { NothingSelectedView } from "../views/NothingSelectedView"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      {/*<Typography>Qui ad ex ea est laborum culpa velit sint mollit incididunt eiusmod non laborum eiusmod.</Typography>*/}
+    
+      <NothingSelectedView/>
+      {/*<NoteView/>*/}
+
+      <IconButton
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor:'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },//Cuando el mouse esta por encima de este elemento
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
+
+
+    </JournalLayout>
     
   )
 }
