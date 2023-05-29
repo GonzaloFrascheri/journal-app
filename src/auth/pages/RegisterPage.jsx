d86a5777ae4eeed643365901e7d749cc8af970f5
Gonzalo <gonzalofrascheri@gmail.com>
2023-05-29 19:45:39 -0300

    Continuación LogIn Firebase

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,51 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { Directions, Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { startCreatingUserWithEmailPassword } from '../../store/auth/thunks';
+
+
+const fromData = {
+  email: '',
+  password: '',
+  displayName: ''
+}
+
+const formValidations = {
+  email: [(value) => value.includes('@') ,'El correo debe de tener un @'],
+  password: [(value) => value.length >= 6 ,'El password debe de tener mas de 6 letras'],
+  displayName: [(value) => value.length >= 1 ,'El nombre es obligatorio'],
+  
+}
 
 export const RegisterPage = () => {
+
+  const dispatch = useDispatch();
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const { formState ,displayName, email, password, onInputChange,
+      isFormValid, displayNameValid, emailValid, passwordValid, 
+  } = useForm(fromData, formValidations);
+
+  
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    
+    if( !isFormValid) return;
+
+    dispatch(startCreatingUserWithEmailPassword(formState));
+    
+
+  }
+
   return (
     <AuthLayout title='Crear cuenta'>
-       <form>
+       <form onSubmit={ onSubmit }>
           <Grid container>
             <Grid item xs= { 12 } sx={{ mt: 2 }}>
               <TextField
@@ -14,6 +53,11 @@ export const RegisterPage = () => {
                 type="text"
                 placeholder="Nombre completo"
                 fullWidth
+                name="displayName"
+                value={ displayName }
+                onChange={ onInputChange }
+                error={ !!displayNameValid && formSubmitted}
+                helperText= { displayNameValid }
               />
 
             </Grid>
@@ -24,6 +68,11 @@ export const RegisterPage = () => {
                 type="text"
                 placeholder="Correo"
                 fullWidth
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+                error={ !!emailValid && formSubmitted}
+                helperText= { emailValid }
               />
 
             </Grid>
@@ -31,16 +80,25 @@ export const RegisterPage = () => {
             <Grid item xs= { 12 } sx={{ mt: 2 }}>
               <TextField
                 label= "Contraseña"
-                type="contraseña"
-                placeholder="correo@google.com"
+                type="password"
+                placeholder="Contraseña"
                 fullWidth
+                name="password"
+                value={ password }
+                onChange={ onInputChange }
+                error={ !!passwordValid && formSubmitted}
+                helperText= { passwordValid }
               />
 
             </Grid>
 
             <Grid container spacing={2 } sx={{ mb: 2, mt: 1}}>
               <Grid item xs={ 12 }>
-                <Button variant="contained" fullWidth>
+                <Button 
+                  type="submit"
+                  variant="contained" 
+                  fullWidth
+                >
                   Crear cuenta
                 </Button>
               </Grid>
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { signInWithGoogle } from "../../firebase/providers";
+import { registerUserWithEmailPassword, signInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials, login, logout } from "./authSlices";
 
 
@@ -19,4 +19,16 @@ export const startGoogleSignIn = () => {
         dispatch(login(result))
         
     } 
+}
+
+export const startCreatingUserWithEmailPassword = ({email, password, displayName}) => {
+    return async(dispatch) => {
+
+        dispatch (checkingCredentials());
+
+        const resp = await registerUserWithEmailPassword({ email, password, displayName});
+
+        console.log(resp);
+
+    }
 }
